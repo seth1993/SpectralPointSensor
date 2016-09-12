530b14ff30632ebf6f8e5d59135dcb609783aaa9
Seth Bailey <Seth Bailey>
2016-09-12 07:46:11 -0600

    Added UI project

---
+++ newSpectro/src/app/app.ts
@@ -0,0 +1,35 @@
+import {Component} from 'angular2/core';
+import {RouteConfig, ROUTER_DIRECTIVES} from 'angular2/router';
+import {FORM_PROVIDERS} from 'angular2/common';
+
+import '../style/app.scss';
+
+import {Api} from './services/api/api';
+import {Home} from './components/home/home';
+import {About} from "./components/about/about";
+import {Status} from "./components/status/status";
+
+/*
+ * App Component
+ * Top Level Component
+ */
+@Component({
+  selector: 'app', // <app></app>
+  providers: [...FORM_PROVIDERS, Api],
+  directives: [...ROUTER_DIRECTIVES],
+  pipes: [],
+  styles: [require('./app.scss')],
+  template: require('./app.html')
+})
+@RouteConfig([
+  {path: '/', component: Home, name: 'Home'},
+  {path: '/About', component: About, name: 'About'}
+])
+export class App {
+  url: string = 'https://github.com/preboot/angular2-universal-windows-app';
+
+  constructor(public api: Api) {
+
+      new Status();
+  }
+}
---
+++ newSpectro/src/app/components/status/status.ts
@@ -0,0 +1,10 @@
+﻿import {Component} from 'angular2/core';
+import {bootstrap} from 'angular2/platform/browser';
+
+
+@Component({
+    selector: 'status',
+    template: '<p>This is status</p>',
+    styles: [require('./status.scss')],
+})
+export class Status {}
